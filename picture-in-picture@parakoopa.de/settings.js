ff0280341553d9acff65a59086198b53e7c1b248
neon12345 <784389+neon12345@users.noreply.github.com>
2023-08-10 07:16:12 +0200

    Update settings.js
    fix

--- picture-in-picture@parakoopa.de/settings.js
+++ picture-in-picture@parakoopa.de/settings.js
@@ -22,12 +22,12 @@ var SETTING_LAST_WINDOW_HASH = "last-window-hash";
 
 var PictureInPictureSettings = GObject.registerClass({
        GTypeName: 'PictureInPicture.settings',
-   }, class PictureInPicture.settings extends GObject.Object {
+   }, class settings extends GObject.Object {
     
     constructor() {
         this._settings = Convenience.getSettings();
         this._settings.connect("changed", Lang.bind(this, this._onChanged));
-    },
+    }
 
     _onChanged(settings, key) {
         // "my-property-name" => myPropertyName
@@ -35,75 +35,75 @@ var PictureInPictureSettings = GObject.registerClass({
             return az.substr(1).toUpperCase();
         });
         this.emit("changed", property);
-    },
+    }
 
     get focusHidden() {
         return this._settings.get_boolean(SETTING_FOCUS_HIDDEN);
-    },
+    }
 
     set focusHidden(value) {
         this._settings.set_boolean(SETTING_FOCUS_HIDDEN, value);
-    },
+    }
 
     get initialZoom() {
         return this._settings.get_double(SETTING_INITIAL_ZOOM);
-    },
+    }
 
     set initialZoom(value) {
         this._settings.set_double(SETTING_INITIAL_ZOOM, value);
-    },
+    }
 
     get initialLeftCrop() {
         return this._settings.get_double(SETTING_INITIAL_LEFT_CROP);
-    },
+    }
 
     set initialLeftCrop(value) {
         this._settings.set_double(SETTING_INITIAL_LEFT_CROP, value);
-    },
+    }
 
     get initialRightCrop() {
         return this._settings.get_double(SETTING_INITIAL_RIGHT_CROP);
-    },
+    }
 
     set initialRightCrop(value) {
         this._settings.set_double(SETTING_INITIAL_RIGHT_CROP, value);
-    },
+    }
 
     get initialTopCrop() {
         return this._settings.get_double(SETTING_INITIAL_TOP_CROP);
-    },
+    }
 
     set initialTopCrop(value) {
         this._settings.set_double(SETTING_INITIAL_TOP_CROP, value);
-    },
+    }
 
     get initialBottomCrop() {
         return this._settings.get_double(SETTING_INITIAL_BOTTOM_CROP);
-    },
+    }
 
     set initialBottomCrop(value) {
         this._settings.set_double(SETTING_INITIAL_BOTTOM_CROP, value);
-    },
+    }
 
     get initialCorner() {
         return this._settings.get_enum(SETTING_INITIAL_CORNER);
-    },
+    }
 
     set initialCorner(value) {
         this._settings.set_enum(SETTING_INITIAL_CORNER, value);
-    },
+    }
 
     get behaviorMode() {
         return this._settings.get_string(SETTING_BEHAVIOR_MODE);
-    },
+    }
 
     set behaviorMode(value) {
         this._settings.set_string(SETTING_BEHAVIOR_MODE, value);
-    },
+    }
 
     get lastWindowHash() {
         return this._settings.get_string(SETTING_LAST_WINDOW_HASH);
-    },
+    }
 
     set lastWindowHash(value) {
         this._settings.set_string(SETTING_LAST_WINDOW_HASH, value);
